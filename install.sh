06b36d3782c7026f559fa520a8155ae9353a726f
johnflowerful@users.noreply.github.com <johnflowerful@users.noreply.github.com>
2023-05-20 17:26:38 +1200

    fix shellcheck warnings

--- install.sh
+++ install.sh
@@ -3,20 +3,20 @@
 #set -x
 MY_ROON_URL="http://download.roonlabs.com/builds/RoonInstaller64.exe"
 MY_ARCH="win64"
-test "$MY_ARCH" = "win32" && MY_ROON_URL="http://download.roonlabs.com/builds/RoonInstaller.exe"
+[[ "$MY_ARCH" = "win32" ]] && MY_ROON_URL="http://download.roonlabs.com/builds/RoonInstaller.exe"
 MY_WINE_ROON_DIR=".roon"
 
-PREFIX="${HOME}/$MY_WINE_ROON_DIR"
-WINE_ENV="env WINEARCH=$MY_ARCH WINEPREFIX=$PREFIX WINEDLLOVERRIDES=winemenubuilder.exe=d"
+PREFIX="${HOME}/${MY_WINE_ROON_DIR}"
+WINE_ENV="env WINEARCH=${MY_ARCH} WINEPREFIX=${PREFIX} WINEDLLOVERRIDES=winemenubuilder.exe=d"
 
 VERBOSE=0
 
 check_executable() {
 	local exe=$1
 
-	if ! command -v $exe &> /dev/null; then
-		echo "ERROR: can't find $exe, which is required for Roon installation."
-		echo "Please install $exe using your distribution package tooling."
+	if ! command -v "${exe}" &>/dev/null; then
+		echo "ERROR: can't find ${exe}, which is required for Roon installation."
+		echo "Please install ${exe} using your distribution package manager."
 		echo
 		exit 1
 	fi
@@ -25,12 +25,12 @@ check_executable() {
 _winetricks() {
 	comment="$1"
 	shift
-	echo "[$MY_ARCH|$PREFIX] $comment ..."
+	echo "[${MY_ARCH}|${PREFIX}] ${comment} ..."
 
-	if [ $VERBOSE -eq 1 ]; then
-		$WINE_ENV winetricks "$@"
+	if [[ "${VERBOSE}" -eq 1 ]]; then
+		${WINE_ENV} winetricks "$@"
 	else
-		$WINE_ENV winetricks "$@" > /dev/null 2>&1
+		${WINE_ENV} winetricks "$@" &>/dev/null
 	fi
 
 	sleep 2
@@ -39,12 +39,12 @@ _winetricks() {
 _wine() {
 	comment="$1"
 	shift
-	echo "[${MY_ARCH}|${PREFIX}] $comment ..."
+	echo "[${MY_ARCH}|${PREFIX}] ${comment} ..."
 
-	if [ $VERBOSE -eq 1 ]; then
-		$WINE_ENV wine "$@"
+	if [[ ${VERBOSE} -eq 1 ]]; then
+		${WINE_ENV} wine "$@"
 	else
-		$WINE_ENV wine "$@" > /dev/null 2>&1
+		${WINE_ENV} wine "$@" &>/dev/null
 	fi
 
 	sleep 2
@@ -58,7 +58,7 @@ check_executable wget
 check_executable xdotool
 
 # configure Wine
-rm -rf $PREFIX
+rm -rf "${PREFIX}"
 _wine "Setup Wine bottle" wineboot --init
 
 #_winetricks "Installing .NET 4.7.2"  -q --force dotnet472
@@ -75,11 +75,11 @@ _winetricks "Disabling crash dialog"        -q nocrashdialog
 sleep 2
 
 # download Roon
-ROON_EXE=$(basename $MY_ROON_URL)
-test -f $ROON_EXE || wget $MY_ROON_URL
+ROON_EXE=$(basename "${MY_ROON_URL}")
+[[ -f "${ROON_EXE}" ]] || wget "${MY_ROON_URL}"
 
 # install Roon
-_wine "Installing Roon" $ROON_EXE
+_wine "Installing Roon" "${ROON_EXE}"
 
 # ensure local bin dir exists
 mkdir -p ~/.local/bin
@@ -87,9 +87,9 @@ mkdir -p ~/.local/bin
 # Preconditions for start script. 
 # Need a properly formatted path to the user's Roon.exe in their wine configuration
 # Get the Windows OS formatted path to the user's Local AppData folder
-WINE_LOCALAPPDATA=$($WINE_ENV wine cmd.exe /c echo %LocalAppData% 2> /dev/null)
+WINE_LOCALAPPDATA=$(${WINE_ENV} wine cmd.exe /c echo %LocalAppData% 2> /dev/null)
 # Convert Windows OS formatted path to Linux formatted path from the user's wine configuration
-UNIX_LOCALAPPDATA="$($WINE_ENV winepath -u "$WINE_LOCALAPPDATA")"
+UNIX_LOCALAPPDATA="$(${WINE_ENV} winepath -u "${WINE_LOCALAPPDATA}")"
 # Windows line endings carry through winepath conversion. Remove it to get an error free path.
 UNIX_LOCALAPPDATA=${UNIX_LOCALAPPDATA%$'\r'} # remove ^M
 ROONEXE="Roon/Application/Roon.exe"
@@ -106,9 +106,9 @@ cat << EOF > ~/.local/bin/start_roon.sh
 
 SCALEFACTOR=1.0
 
-PREFIX="$PREFIX"
+PREFIX="${PREFIX}"
 EXE="${UNIX_LOCALAPPDATA}/${ROONEXE}"
-env WINEPREFIX=\$PREFIX WINEDEBUG=fixme-all wine \$EXE -scalefactor=\$SCALEFACTOR
+env WINEPREFIX=\${PREFIX} WINEDEBUG=fixme-all wine \${EXE} -scalefactor=\${SCALEFACTOR}
 EOF
 chmod +x ~/.local/bin/start_roon.sh
 
